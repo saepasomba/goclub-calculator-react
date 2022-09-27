c80a5c27c4129af7e4fdcb1392102c16a75a66b8
Sae Pasomba <saepasomba12@gmail.com>
2022-09-27 09:58:08 +0700

    Add some basic css and header

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+
+  return (
+    <header>
+      <div className='container container-header'>
+        <h1><span className='purple'>GoClub</span> Calculator</h1>
+        <p>How much do you need to be a <span className='green'>sultan</span>?</p>
+      </div>
+    </header>
+  )
+}
---
+++ src/Pages/App.js
@@ -0,0 +1,13 @@
+
+import './App.scss';
+import Header from '../Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
